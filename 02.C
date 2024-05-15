b39ea45a977c3b2edff11b9fdc631137fe951760
Eloiza <168553661+Eloiza15@users.noreply.github.com>
2024-05-15 10:37:20 -0300

    02.C

---
+++ 02.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+	float nota1;
+	float nota2;
+	float nota3;
+	float nota4;
+	
+	printf("Digite a primeira nota:");
+	scanf("%f", &nota1);
+	printf("Digite a segunda nota:");
+	scanf("%f", &nota2);
+	printf("Digite a terceira nota:");
+	scanf("%f", &nota3);
+	printf("Digite a quarta nota:");
+	scanf("%f", &nota4);
+	
+	float total = nota1+nota2+nota3+nota4;
+	float media = (total/4);
+	
+	printf("\nNota total: %f", total);
+	printf("\n\nMedia: %f", media);
+	
+	return 0;
+}
